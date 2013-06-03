f67ba53f81b772081de0c6668bd3347ad818a40b
Matthias Nott <matthias.nott@sap.com>
2013-06-04 01:16:10 +0200

    Initial version.
    Former-commit-id: 42c0650190cf14894a225f306f3280c82233214f
    Former-commit-id: 038393193c6954bbef6882c4469ced0a3f3e48c7

---
+++ cfg/rename.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo Usage: #0 <remote_repository_name>
+  exit 1;
+fi
+
+git remote rename origin upstream
+git remote add origin git:$1
+git push origin master
+git branch master --set-upstream-to origin/master
